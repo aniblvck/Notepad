906f83db4b71980e33f9662c4b72d335c015da72
anirudh khade <anirudh24io010@satiengg.in>
2024-06-12 13:35:54 -0700

    working on filedata

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,18 @@ router.get('/', function(req, res, next) {
 router.get('/files', function(req, res, next) {
 const files=fs.readdirSync(path.join(gpath),"utf-8")
 
-  res.render('files',{files: files});
+  res.render('files',{files: files });
 });
 
 router.get('/readmore', function(req, res, next) {
-  res.redirect('/files');
+  res.render('readmore',{files:''});
+});
+
+
+
+router.get('/readmore/:filename', function(req, res, next) {
+  const filedata=fs.readFileSync(path.join(gpath,req.params.filename),'utf8')
+  res.redirect('readmore',{filename:req.params.filename,filedata: req.params.filedata});
 });
 
 
